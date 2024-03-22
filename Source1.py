3de0c207827fe6d6a30ed74af1999db7ecded6cf
Mikkel-AI0700 <mikeruai0700@gmail.com>
2024-03-22 14:41:25 +0800

    New commit

---
+++ Source1.py
@@ -0,0 +1,14 @@
+import pickle
+import pandas as pd
+from MainPackage import variables as var
+
+class Main:
+
+        class LoadModel:
+                pass
+
+        class InteractModel:
+                pass
+
+def main ():
+        pass
