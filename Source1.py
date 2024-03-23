d0c8c6c346dbef8b3fc6319e070ef13301aaa014
Mikkel-AI0700 <mikeruai0700@gmail.com>
2024-03-23 12:26:08 +0800

    The effing for loop finally worked hahahaha. Intellisense, why?

--- Source1.py
+++ Source1.py
@@ -5,10 +5,79 @@ from MainPackage import variables as var
 class Main:
 
         class LoadModel:
-                pass
+                
+                def __init__ (self):
+                        self.Interaction_Class_Access = Main.InteractModel()
+
+                def Load (self):
+                        with open ("/home/mikkel-ai/Desktop/Machine Learning/Heart Failure Classifier/Project-HeartFailureClassifier/Model/DTC_Model.pkl", "rb") as DTC_Model_Dump:
+                                self.Interaction_Class_Access.DTC_Model = pickle.load(DTC_Model_Dump)
 
         class InteractModel:
-                pass
+                
+                def __init__ (self):
+                        self.DTC_Model = None
+                        self.Error_Dictionary = {
+                                "IDT_NOT_DESIRED_DATATYPE_ERROR": "[-] Error: One of the values is not a string, number or float",
+                                "VE_NOT_DESIRED_INPUT": "[-] Error: One of the input is not one of the following: Yes, No, Male, Female"
+                        }
+
+                def Validate_Inputs (self):
+
+                        for BinaryValue in DictionaryVariables["Binary_Variables"].values():
+                                pass
 
 def main ():
-        pass
+        
+        MainClass = Main()
+
+        global DictionaryQuestions
+        global DictionaryVariables
+
+        DictionaryQuestions = {
+                "Binary_Question": {
+                        "Anemia_Question": var.If_Patient_Has_Anemia,
+                        "Diabetes_Question": var.If_Patient_Has_Diabetes,
+                        "HBP_Question": var.If_Patient_Has_HBP,
+                        "Gender_Question": var.If_Patient_Is_Male_Or_Female,
+                        "Smoking_Question": var.If_Patient_Is_Smoking
+                },
+                "Integer_Continuous_Question": {
+                        "Age_Question": var.Patient_Age_Question,
+                        "Creatinine_Phospho_Question": var.Patient_Creatinine_Phospho_Question,
+                        "Ejection_Fraction_Question": var.Patient_Ejection_Fraction_Question,
+                        "Serum_Sodium_Question": var.Patient_Serum_Sodium_Question,
+                        "Time_Question": var.Patient_Time,
+                        "Platelets_Question": var.Patient_Platelets_Amount,
+                        "Serum_Creatinine_Question": var.Patient_Serum_Creatinine
+                }
+        }
+
+        DictionaryVariables = {
+                "Binary_Variables": {
+                        "Anemia_Variable": var.PatientAnemia,
+                        "Diabetes_Variable": var.PatientDiabetes,
+                        "HBP_Variable": var.PatientHBP,
+                        "Gender_Variable": var.PatientSex,
+                        "Smoking_Variable": var.PatientSmoking
+                },
+                "Integer_Continuous_Variables": {
+                        "Age_Variable": var.PatientAge,
+                        "Creatinine_Phospho_Variable": var.PatientCreatininePhospo,
+                        "Ejection_Fraction_Variable": var.PatientEjectionFrac,
+                        "Serum_Sodium_Variable": var.PatientSerumSodium,
+                        "Time_Variable": var.PatientTime,
+                        "Platelets_Variable": var.PatientPlatelets,
+                        "Serum_Creatinine_Variable": var.PatientSerumCreatinine
+                }
+        }
+
+        for (PatientQuestion, CurrentlyLoopedKey) in zip(DictionaryQuestions["Binary_Question"].values(), DictionaryVariables["Binary_Variables"].keys()):
+                UserAnswer = input(PatientQuestion)
+                DictionaryVariables["Binary_Variables"].update({CurrentlyLoopedKey: UserAnswer})
+
+        for (PatientQuestion, CurrentlyLoopedKey) in zip(DictionaryQuestions["Integer_Continuous_Question"].values(), DictionaryVariables["Integer_Continuous_Variables"].keys()):
+                UserAnswer = input(PatientQuestion)
+                DictionaryVariables["Integer_Continuous_Variables"].update({CurrentlyLoopedKey: UserAnswer})
+
+main()
